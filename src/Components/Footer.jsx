d508fe6782aba0ac81c1a35a8f23e09f6ee7c880
Gabrielle Jacobs <GabbyJ424@gmail.com>
2020-11-01 19:01:38 -0600

    Add design inspiration to footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,8 @@ import { FaReact } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer>
-      Made with <FaReact className="icon" /> for the Tandem 2020 Apprenticeship Code Challenge by Gabrielle Jacobs
+      Made with <FaReact className="icon" /> for the Tandem 2020 Apprenticeship Code Challenge by Gabrielle Jacobs<br/>
+      Design Inspired by <a href="https://madeintandem.com" target="_blank" rel="noreferrer">Tandem's website</a>
     </footer>
   );
 }
